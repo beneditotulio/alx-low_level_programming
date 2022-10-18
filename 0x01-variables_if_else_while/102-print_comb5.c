e7bb83c9798455e76c288ec5519b72800b9954b0
beneditotulio <80028633+beneditotulio@users.noreply.github.com>
2022-10-18 10:07:02 +0200

    Update 102-print_comb5.c
    Fixing Betty Coding Style - 12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print single digit numbers
+ * main - print pairs of double digit combos
  *
  * Description: print pairs of double digit combos
  *
@@ -12,37 +12,37 @@ int main(void)
 {
 	int i, j, k, m;
 
-		i = 48;
-		while (i < 58)
+	i = 48;
+	while (i < 58)
+	{
+		j = 48;
+		while (j < 58)
 		{
-			j = 48;
-			while (j < 58)
+			m = j + 1;
+			k = i;
+			while (k < 58)
 			{
-				m = j + 1;
-				k = i;
-				while (k < 58)
+				while (m < 58)
 				{
-					while (m < 58)
+					putchar(i);
+					putchar(j);
+					putchar(32);
+					putchar(k);
+					putchar(m);
+					if (i < 57 || j < 56 || k < 57 || m < 57)
 					{
-						putchar(i);
-						putchar(j);
+						putchar(44);
 						putchar(32);
-						putchar(k);
-						putchar(m);
-						if (i < 57 || j < 56 || k < 57 || m < 57)
-						{
-							putchar(44);
-							putchar(32);
-						}
-						m++;
 					}
-					m = 48;
-					k++;
+					m++;
 				}
-				j++;
+				m = 48;
+				k++;
 			}
-			i++;
+			j++;
 		}
-		putchar(10);
-		return (0);
+		i++;
+	}
+	putchar(10);
+	return (0);
 }
